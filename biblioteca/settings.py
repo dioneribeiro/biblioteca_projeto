c5e496ad88b24f4709fc413e9a8a9242688db819
dioneribeiro <dioneribeiron@gmail.com>
2024-10-03 23:10:27 -0300

    Alteração do Procfile e setings

--- biblioteca/settings.py
+++ biblioteca/settings.py
@@ -153,6 +153,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
+BASE_DIR = Path(__file__).resolve().parent.parent
+
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
